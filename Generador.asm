17ffd92b82653ede257ba9e35fb16cea492d5886
DRAIXCOR <123282785+DRAIXCOR@users.noreply.github.com>
2023-05-22 01:14:44 -0600

    Update Generador.asm

--- Generador.asm
+++ Generador.asm
@@ -5,58 +5,43 @@ section .bss
 section .text
 extern printf
 
-global multiplicacion
+global sumar
 , main
 
-multiplicacion:
-		PUSH rbp
-		MOV rbp, rsp
-		SUB rsp, 48
+sumar:
+        PUSH rbp
+        MOV rbp, rsp
+        SUB rsp, 48
+
 MOV QWORD [rbp -24], rdi
 
-MOV QWORD [rbp -28], rsi
+MOV QWORD [rbp -28], rdi
 
-		MOV rax, QWORD [rbp -28]
+MOV rax, QWORD [rbp -24]
 
-		MOV rdi, QWORD [rbp -24]
 
-		ADD rax, rdi
+        ADD rsp, 48
+        MOV rsp, rbp
+        POP rbp
+        ret
 
-		ADD rsp, 48
-		MOV rsp, rbp
-		POP rbp
-		ret
 
-main: 
-		PUSH rbp
-		MOV rbp, rsp
-		SUB rsp, 48
-		MOV WORD [rbp -4] , 8
-		MOV WORD [rbp -8] , 4
-		MOV rax, QWORD [rbp -4]
-		MOV rdi, rax
-		MOV rax, QWORD [rbp -8]
-		MOV rsi, rax
+main:
+        PUSH rbp
+        MOV rbp, rsp
+        SUB rsp, 48
 
-		call multiplicacion
+        MOV rdi, rax
 
-		MOV QWORD [rbp -8], rax
-		PUSH QWORD [rbp -8]
-		FILD DWORD [rsp]
-		FILD QWORD [rel resp]
-		ADD rsp, 8
-		MOVSD xmm0, QWORD [rel resp]
-		MOD rdi, primr
-		mov al, 1
-		
-		call printf WRT ..plt
+        call sumar
+        MOV rdi, rax
 
-		ADD rsp, 48
-		MOV rsp, rbp
-		MOV rax, 60
-		MOV rdi
-		syscall
+        call sumar
 
+        ADD rsp, 48
+        MOV rsp, rbp
+        POP rbp
+        ret
 
 
 
